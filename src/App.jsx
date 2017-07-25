c62bfbd41cc0562aea691e7ecf5517af0a4f37af
Tyler Krys <tylerkrys@gmail.com>
2017-07-25 20:22:46 +0000

    Adds hard-coded app data object into variable appData, passes state to <MessageList/> and <ChatBar/>

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import ChatBar from './ChatBar.jsx';
 import Message from './Message.jsx';
 import MessageList from './MessageList.jsx';
 
+const appData = {
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {loading: false};
+    this.state = appData;
   }
 
   render() {
     console.log("Rendering <App/>");
     return (
       <div>
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={this.state.messages} />
+        <ChatBar currentUser={this.state.currentUser} />
       </div>
     );
   }
