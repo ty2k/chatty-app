22bdfad49091af1389f7d946714a183ee5b1d1cf
Tyler Krys <tylerkrys@gmail.com>
2017-07-26 01:13:33 +0000

    Refactors MessageList.jsx to include the <Message/> component with a unique key

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -5,16 +5,13 @@ class MessageList extends Component {
   render() {
     console.log("Rendering <MessageList/>");
     const messagesArray = this.props.messages;
-    const messagesDiv = messagesArray.map((message) =>
-      <div key={message.id}>
-        <span className="message-username">{message.username}</span>
-        <span className="message-content">{message.content}</span>
-      </div>
+    const messagesMap = messagesArray.map((message) =>
+      <Message message={message} key={message.id}/>
     );
     return (
       <main className="messages">
         <div>
-          {messagesDiv}
+          {messagesMap}
         </div>
       </main>
     );
