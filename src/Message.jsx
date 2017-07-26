2013edb0913b373d76d7da9a11ae9a964fadff4e
Tyler Krys <tylerkrys@gmail.com>
2017-07-26 01:12:58 +0000

    Removes hard-coded messages from inside Message.jsx, adds references to {this.props} to get messages dynamically

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,14 +4,9 @@ class Message extends Component {
   render() {
     console.log("Rendering <Message/>");
     return (
-      <div>
-        <div className="message">
-          <span className="message-username">Anonymous1</span>
-          <span className="message-content">I won't be impressed with technology until I can download food.</span>
-        </div>
-        <div className="message system">
-          Anonymous1 changed their name to nomnom.
-        </div>
+      <div className="message">
+        <span className="message-username">{this.props.message.username}</span>
+        <span className="message-content">{this.props.message.content}</span>
       </div>
     );
   }
