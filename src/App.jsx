2e4bf6f1dec55b8c9f2e09936b3d0720d9d78c65
Tyler Krys <tylerkrys@gmail.com>
2017-07-27 18:01:56 +0000

    Adds type attribute with value postMessage to addMessage() function

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ class App extends Component {
 
   addMessage(message) {
     const newMessage = {
+      type: "postMessage",
       id: new Date(),
       username: this.state.currentUser.name,
       content: message
